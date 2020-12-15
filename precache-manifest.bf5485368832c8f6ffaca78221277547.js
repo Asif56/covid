0d6e9f5a1d0eefb3110674a00dd436e53c9ee2e6
Asif56 <asif56175@gmail.com>
2020-12-15 19:28:55 +0530

    Updates

---
+++ precache-manifest.bf5485368832c8f6ffaca78221277547.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f07ec4e98f9dc43ab2c901f7080686",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "59390dcb34eef89e4365",
+    "url": "/covid/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "f7bfc6b28e02ee308cd3",
+    "url": "/covid/static/css/main.8829b918.chunk.css"
+  },
+  {
+    "revision": "59390dcb34eef89e4365",
+    "url": "/covid/static/js/2.3b53e95e.chunk.js"
+  },
+  {
+    "revision": "a8b062ea113d5347eaa037e7d1931e01",
+    "url": "/covid/static/js/2.3b53e95e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7bfc6b28e02ee308cd3",
+    "url": "/covid/static/js/main.5fc63add.chunk.js"
+  },
+  {
+    "revision": "062044bcf29db4febcc2",
+    "url": "/covid/static/js/runtime-main.45dcbfad.js"
+  }
+]);
